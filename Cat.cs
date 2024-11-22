bbcdb2ca9cbe177c23c7ecf0730217ccdd1850a0
georgeckito <gigickitishvili@gmail.com>
2024-11-22 07:54:46 -0800

    init

---
+++ Cat.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26noemberi
+{
+    internal class Cat
+    {
+        private int grams;
+        public string name { get; set; }
+        public string breed { get; set; }
+        public uint age { get; set; }
+        public string sex { get; set; }
+
+        public Cat() { }
+        public Cat(string name, string breed, uint age, string sex)
+        {
+            this.name = name;
+            this.breed = breed;
+            this.age = age;
+            this.sex = sex;
+        }
+        public void Meow(int count)
+        {
+            for(int i = 0; i < count; i++)
+            {
+                Console.WriteLine("Meow");
+            }
+        }
+
+        public void Eat(int grams)
+        {
+            Console.WriteLine($"{name} eating");
+            for(int i = 0; i < Math.Floor((double) grams / 10); i++)
+            {
+                Console.WriteLine("eating");
+            }
+            this.grams = grams; // ?
+            Console.WriteLine($"{name} finish eating");
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using _26noemberi;
+
+Console.WriteLine("creating object");
+Console.Write("name: ");
+string name = Console.ReadLine();
+Console.Write("age: ");
+int age = int.Parse(Console.ReadLine());
+Console.Write("breed: ");
+string breed = Console.ReadLine();
+Console.Write("sex: ");
+string sex = Console.ReadLine();
+if (!new string[2] { "male", "female" }.Contains(sex))
+{
+    Console.WriteLine("wrong input, male or female");
+    return;
+}
+Cat cat = new Cat(name, breed, (uint) age, sex);
+Console.WriteLine("\ncreated");
+Console.Write("enter grams: ");
+int grams = int.Parse(Console.ReadLine());
+cat.Eat(grams);
+Console.Write("\nenter meow count: ");
+int count = int.Parse(Console.ReadLine());
+cat.Meow(count);
